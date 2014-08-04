35d37c0ac59edc9ff026e01e4a372617a5e359ae
Markus Jura <markus.jura@gmail.com>
2014-08-04 10:20:42 +0200

    Render Default Stocks

--- app/actors/Stock.scala
+++ app/actors/Stock.scala
@@ -1,16 +1,21 @@
 package actors
 
-import akka.actor.{Props, Actor}
-import play.api.Logger
+import akka.actor._
 import utils.StockQuote
 import scala.util.Random
 import scala.collection.immutable.Queue
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.Play.current
 
 object Stock {
   def props(symbol: String) = Props(new Stock(symbol))
 
   // Message Protocol
   trait Message
+  case object FetchLatest extends Message
+  case class Update(symbol: String, price: Double) extends Message
+  case class History(symbol: String, history: Queue[Double]) extends Message
   case class Watch(symbol: String) extends Message
   case object Unwatch extends Message
 }
@@ -20,6 +25,9 @@ object Stock {
  * values. Each Stock actour updates a rolling dataset of randomly generated stock values.
  */
 class Stock(symbol: String) extends Actor {
+  import Stock._
+
+  var watchers: Set[ActorRef] = Set.empty[ActorRef]
 
   // A random data set which uses stockQuote.newPrice to get each data point
   var stockHistory: Queue[Double] = {
@@ -27,8 +35,21 @@ class Stock(symbol: String) extends Actor {
     initialPrices.take(50).to[Queue]
   }
 
+  // Fetch the latest stock value every 75ms
+  val stockTick = context.system.scheduler.schedule(Duration.Zero, 75.millis, self, FetchLatest)
+
   def receive = {
-    case message => Logger.debug(s"Stocks actor received message $message")
+    case FetchLatest =>
+      // Add a new stock price to the history and drop the oldest
+      val newPrice = updateStockHistory
+      // Notify watchers
+      watchers.foreach(_ ! Stock.Update(symbol, newPrice))
+
+    case Watch(_) =>
+      // Send the stock history to the user
+      sender ! History(symbol, stockHistory)
+      // Add the watcher to the list
+      watchers = watchers + sender
   }
 
   // Add a new stock price to the history and drop the oldest
--- app/actors/User.scala
+++ app/actors/User.scala
@@ -3,6 +3,8 @@ package actors
 import akka.actor._
 import play.api.Logger
 import play.api.libs.json._
+import scala.collection.JavaConversions._
+import play.api.Play.current
 
 object User {
   def props(out: ActorRef) = Props(new User(out))
@@ -14,13 +16,27 @@ object User {
  */
 class User(out: ActorRef) extends Actor {
 
-  val initMsg = "WebSocket connection established"
-  Logger.debug(initMsg)
-  out ! Json.obj("msg" -> initMsg)
+  val defaultStocks = play.api.Play.configuration.getStringList("default.stocks").get
+  // New user watches default stocks
+  defaultStocks.foreach(symbol => StocksDelegator.ref ! Stock.Watch(symbol))
 
   def receive = {
     case message: JsValue =>
       Logger.debug(s"New WebSocket message: $message")
       out ! Json.obj("msg" -> message)
+
+    case Stock.Update(symbol, price) =>
+      val message = Json.obj(
+        "type" -> "stockupdate",
+        "symbol" -> symbol,
+        "price" -> price)
+      out ! message
+
+    case Stock.History(symbol, history) =>
+      val message = Json.obj(
+        "type" -> "stockhistory",
+        "symbol" -> symbol,
+        "history" -> history)
+      out ! message
   }
 }
